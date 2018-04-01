4354885ab7eeef3937f55d132e1cfc952117a37f
Geoffrey Fox <geoffrey_fox@hotmail.com>
2018-03-31 18:19:48 -0700

    Added Db Entities and Connection String

---
+++ MySql_Image/Data/ProductImageContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySql_Image.Data
+{
+    public class ProductImageContext : DbContext
+    {
+        public ProductImageContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<ProductImageContext> ProductImages { get; set; }
+
+        //TODO: UpdateTimestamp code
+    }
+}
--- MySql_Image/Startup.cs
+++ MySql_Image/Startup.cs
@@ -4,32 +4,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using MySql_Image.Data;
 
 namespace MySql_Image
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        private readonly IConfiguration _config;
+        private readonly IHostingEnvironment _env;
+        private readonly string _dbconnect = null;
 
-        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration config, IHostingEnvironment env)
+        {
+            _config = config;
+            _env = env;
+            _dbconnect = _config["dbconnect"];
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            if (_env.IsDevelopment())
+            {
+                services.AddDbContext<ProductImageContext>(options => 
+                options.UseMySql(_config["dbconnect"]));
+            }
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (_env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
