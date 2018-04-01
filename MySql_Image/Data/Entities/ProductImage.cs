2d8dfa7e5aeead416f08f65705f257ef83493631
Geoffrey Fox <geoffrey_fox@hotmail.com>
2018-03-31 18:19:48 -0700

    Add Db entities and connection string

---
+++ MySql_Image/Data/Entities/ProductImage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySql_Image.Data.Entities
+{
+    public class ProductImage
+    {
+        public uint Id { get; set; }
+        public DateTime? DateUpdated { get; set; }
+        public string FileName { get; set; }
+        public byte[] ImageThumb { get; set; }
+        public byte[] ImageFull { get; set; }
+    }
+}
