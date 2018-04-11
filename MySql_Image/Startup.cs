1040b44aea4b3afd8a0a1245357d5eb222f1311f
FoxGeoff <fox.geoffrey@gmail.com>
2018-04-11 13:32:44 -0700

    Add Swagger for api

--- MySql_Image/Startup.cs
+++ MySql_Image/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using MySql_Image.Data;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MySql_Image
 {
@@ -35,6 +36,29 @@ namespace MySql_Image
             services.AddTransient<ProductImageSeeder>();
 
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Audio Impact API",
+                    Description = "An example ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact
+                    {
+                        Name = "Geoff Fox",
+                        Email = "geoff@fox-graphics.com",
+                        Url = "https://fox-Graphics.com"
+                    },
+                    License = new License
+                    {
+                        Name = "Use under LICX",
+                        Url = "https://example.com/license"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +74,15 @@ namespace MySql_Image
             }
 
             app.UseStaticFiles();
+            /* http://localhost:50254/swagger/ */
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                //c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc(route =>
             {
